13caba4a657fac9fea273b33cfb5d0303b1d387c
Lucas Yepez <lucasyepez99@gmail.com>
2025-01-22 21:26:01 -0600

    debug typo

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ const User = require('./models/user');
 
 const corsOptions = {
 
-    origin: "https://vitalsource-frontend-k03zxevtb-lucas-projects-f61d5cb5.vercel.app",
-    "https://vitalsource-frontend-a2qq06694-lucas-projects-f61d5cb5.vercel.app/",
+    origin: ["https://vitalsource-frontend-k03zxevtb-lucas-projects-f61d5cb5.vercel.app",
+        "https://vitalsource-frontend-a2qq06694-lucas-projects-f61d5cb5.vercel.app/"
+    ],
     // Allow only this origin
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
